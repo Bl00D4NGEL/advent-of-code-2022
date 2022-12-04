4f51ad8176d24e21e32154f1f6a812185c91a4ea
Bl00D4NGEL <kuhlesdominik@gmx.de>
2022-12-04 19:21:49 +0100

    Day 2 part 2

--- day02/main.rs
+++ day02/main.rs
@@ -42,6 +42,11 @@ fn main() {
     let contents =
         fs::read_to_string("./input.txt").expect("Should have been able to read the file");
 
+    part_one(contents.clone());
+    part_two(contents);
+}
+
+fn part_one(contents: String) {
     // Split file by new line
     let lines = contents.split("\n");
 
@@ -58,7 +63,7 @@ fn main() {
             _ => panic!("Cannot match move"),
         };
 
-        let my_move = match &moves.next() {
+        let my_move = match moves.next() {
             None => panic!("My move must exist"),
             Some("X") => PossibleMoves::ROCK,
             Some("Y") => PossibleMoves::PAPER,
@@ -79,8 +84,60 @@ fn main() {
         };
 
         sum_points = sum_points + move_based_points + result_based_points;
-        // let enemey_move = match moves{}
     }
 
-    println!("Sum points: {:?}", sum_points);
+    println!("Part 1: Sum points: {:?}", sum_points);
+}
+
+fn part_two(contents: String) {
+    // Split file by new line
+    let lines = contents.split("\n");
+
+    let mut sum_points = 0;
+
+    for line in lines {
+        let mut moves = line.split(" ");
+
+        let enemy_move = match moves.next() {
+            None => panic!("Enemy move must exist"),
+            Some("A") => PossibleMoves::ROCK,
+            Some("B") => PossibleMoves::PAPER,
+            Some("C") => PossibleMoves::SCISSORS,
+            _ => panic!("Cannot match move"),
+        };
+
+        let expected_result = match &moves.next() {
+            None => panic!("My move must exist"),
+            Some("X") => GameResult::LOSE,
+            Some("Y") => GameResult::DRAW,
+            Some("Z") => GameResult::WIN,
+            _ => panic!("Cannot match move"),
+        };
+
+        let move_for_result;
+
+        if PossibleMoves::ROCK.play_against(&enemy_move) == expected_result {
+            move_for_result = PossibleMoves::ROCK;
+        } else if PossibleMoves::PAPER.play_against(&enemy_move) == expected_result {
+            move_for_result = PossibleMoves::PAPER;
+        } else {
+            move_for_result = PossibleMoves::SCISSORS;
+        }
+
+        let move_based_points = match &move_for_result {
+            PossibleMoves::ROCK => 1,
+            PossibleMoves::PAPER => 2,
+            PossibleMoves::SCISSORS => 3,
+        };
+
+        let result_based_points = match expected_result {
+            GameResult::LOSE => 0,
+            GameResult::DRAW => 3,
+            GameResult::WIN => 6,
+        };
+
+        sum_points = sum_points + move_based_points + result_based_points;
+    }
+
+    println!("Part 2: Sum points: {:?}", sum_points);
 }
