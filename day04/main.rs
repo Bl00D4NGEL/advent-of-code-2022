dcf1ccd227d25d5403fb326ad9a606280f123af8
Bl00D4NGEL <kuhlesdominik@gmx.de>
2022-12-04 23:00:35 +0100

    Day 4 part 2

--- day04/main.rs
+++ day04/main.rs
@@ -1,19 +1,13 @@
 use core::panic;
-use std::{
-    fs,
-    ops::{Range, RangeInclusive},
-};
+use std::{fs, ops::RangeInclusive};
 
 fn main() {
     let contents =
         fs::read_to_string("./input.txt").expect("Should have been able to read the file");
 
-    task_one(contents);
-}
-
-fn task_one(contents: String) {
     let lines = contents.split("\n");
-    let mut overlaps = 0;
+    let mut full_overlaps = 0;
+    let mut partial_overlaps = 0;
     for line in lines {
         let mut pairs = line.split(",");
 
@@ -24,19 +18,44 @@ fn task_one(contents: String) {
         let pair_one_range = get_range_from_string(pairs.next().unwrap());
         let pair_two_range = get_range_from_string(pairs.next().unwrap());
 
-        // If the start and the end are contained in the other range the full range must be in it
-        if pair_one_range.contains(&pair_two_range.start())
-            && pair_one_range.contains(&pair_two_range.end())
-        {
-            overlaps = overlaps + 1;
-        } else if pair_two_range.contains(&pair_one_range.start())
-            && pair_two_range.contains(&pair_one_range.end())
-        {
-            overlaps = overlaps + 1;
+        if fully_overlaps(&pair_one_range, &pair_two_range) {
+            full_overlaps = full_overlaps + 1;
+        }
+        if partially_overlaps(&pair_one_range, &pair_two_range) {
+            partial_overlaps = partial_overlaps + 1;
         }
     }
 
-    println!("Part 1 overlaps: {:?}", overlaps);
+    println!("Part 1 overlaps: {:?}", full_overlaps);
+    println!("Part 2 overlaps: {:?}", partial_overlaps);
+}
+
+fn fully_overlaps(first: &RangeInclusive<i32>, second: &RangeInclusive<i32>) -> bool {
+    if first.contains(&second.start()) && first.contains(&second.end()) {
+        return true;
+    }
+
+    if second.contains(&first.start()) && second.contains(&first.end()) {
+        return true;
+    }
+
+    return false;
+}
+
+fn partially_overlaps(first: &RangeInclusive<i32>, second: &RangeInclusive<i32>) -> bool {
+    if first.contains(&second.start()) {
+        return true;
+    }
+
+    if first.contains(&second.end()) {
+        return true;
+    }
+
+    if second.contains(&first.start()) {
+        return true;
+    }
+
+    return second.contains(&first.end());
 }
 
 fn get_range_from_string(input: &str) -> RangeInclusive<i32> {
