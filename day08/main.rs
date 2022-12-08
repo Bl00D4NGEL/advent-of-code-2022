f42cff54d3f68a5f10d30f8b62cdd89345f91561
Bl00D4NGEL <kuhlesdominik@gmx.de>
2022-12-08 15:28:58 +0100

    Day 8 part 2

--- day08/main.rs
+++ day08/main.rs
@@ -1,4 +1,5 @@
-use std::{fs, slice::Iter, usize};
+use std::{fs, usize};
+
 #[derive(Debug)]
 struct Forest {
     trees: Vec<u32>,
@@ -23,17 +24,20 @@ impl Forest {
         Forest { trees, length }
     }
 
-    pub fn get_trees_above(&self, index: usize) -> Vec<u32> {
+    pub fn get_trees_above(&self, index: usize) -> Vec<&u32> {
         let tree_position = self.get_tree_position(index);
         let mut trees = vec![];
         for i in 0..tree_position.row {
             trees.push(self.get_tree_at_position(TreePosition::new(tree_position.column, i)));
         }
 
+        // We need to reverse the list of trees because we want to keep the "looking" order of the trees
+        trees.reverse();
+
         trees
     }
 
-    pub fn get_trees_below(&self, index: usize) -> Vec<u32> {
+    pub fn get_trees_below(&self, index: usize) -> Vec<&u32> {
         let tree_position = self.get_tree_position(index);
         let mut trees = vec![];
         // Current row + 1 because we want the rows below the current tree and not include the row of the current tree
@@ -44,7 +48,7 @@ impl Forest {
         trees
     }
 
-    pub fn get_trees_to_the_right_of(&self, index: usize) -> Vec<u32> {
+    pub fn get_trees_to_the_right_of(&self, index: usize) -> Vec<&u32> {
         let tree_position = self.get_tree_position(index);
         let mut trees = vec![];
         // Current column + 1 because we want the columns to the right of the current tree and not include the column of the current tree
@@ -55,21 +59,32 @@ impl Forest {
         trees
     }
 
-    pub fn get_trees_to_the_left_of(&self, index: usize) -> Vec<u32> {
+    pub fn get_trees_to_the_left_of(&self, index: usize) -> Vec<&u32> {
         let tree_position = self.get_tree_position(index);
         let mut trees = vec![];
         for i in 0..tree_position.column {
             trees.push(self.get_tree_at_position(TreePosition::new(i, tree_position.row)));
         }
 
+        // We need to reverse the list of trees because we want to keep the "looking" order of the trees
+        trees.reverse();
+
         trees
     }
 
-    fn get_tree_at_position(&self, position: TreePosition) -> u32 {
+    fn get_tree_at_position(&self, position: TreePosition) -> &u32 {
         let tree = self.trees.get(position.row * self.length + position.column);
         match tree {
             None => panic!("Could not find tree at position {:?}", position),
-            Some(v) => *v,
+            Some(v) => v,
+        }
+    }
+
+    fn get_tree_at_index(&self, index: usize) -> &u32 {
+        let tree = self.trees.get(index);
+        match tree {
+            None => panic!("Could not find tree at index {:?}", index),
+            Some(v) => v,
         }
     }
 
@@ -105,12 +120,52 @@ fn main() {
             .trees
             .iter()
             .enumerate()
-            .filter(|(index, heigth)| is_tree_visible(&forest, *index, **heigth))
+            .filter(|(index, _)| is_tree_visible(&forest, *index))
             .count()
     );
+
+    let mut scenic_secores = forest
+        .trees
+        .iter()
+        .enumerate()
+        .map(|(index, _)| get_scenic_score_for_tree(&forest, index))
+        .collect::<Vec<u32>>();
+
+    scenic_secores.sort();
+    println!("Day 8 part 2: {:?}", scenic_secores.pop());
 }
 
-fn is_tree_visible(forest: &Forest, tree_index: usize, tree_heigth: u32) -> bool {
+fn get_scenic_score_for_tree(forest: &Forest, tree_index: usize) -> u32 {
+    let tree_heigth = forest.get_tree_at_index(tree_index);
+
+    let scenic_scores = vec![
+        get_scenic_score_for_allotment(tree_heigth, &forest.get_trees_above(tree_index)),
+        get_scenic_score_for_allotment(tree_heigth, &forest.get_trees_to_the_left_of(tree_index)),
+        get_scenic_score_for_allotment(tree_heigth, &forest.get_trees_below(tree_index)),
+        get_scenic_score_for_allotment(tree_heigth, &forest.get_trees_to_the_right_of(tree_index)),
+    ];
+
+    match scenic_scores.into_iter().reduce(|acc, score| acc * score) {
+        None => 0,
+        Some(v) => v,
+    }
+}
+
+fn get_scenic_score_for_allotment(tree_heigth: &u32, other_trees: &Vec<&u32>) -> u32 {
+    let mut scenic_score = 0;
+    for tree in other_trees {
+        scenic_score = scenic_score + 1;
+
+        if *tree >= tree_heigth {
+            break;
+        }
+    }
+
+    scenic_score
+}
+
+fn is_tree_visible(forest: &Forest, tree_index: usize) -> bool {
+    let tree_heigth = forest.get_tree_at_index(tree_index);
     if is_tree_visible_in_allotment(tree_heigth, &mut forest.get_trees_above(tree_index)) {
         return true;
     }
@@ -132,7 +187,7 @@ fn is_tree_visible(forest: &Forest, tree_index: usize, tree_heigth: u32) -> bool
     )
 }
 
-fn is_tree_visible_in_allotment(tree_heigth: u32, other_tree_heigths: &mut Vec<u32>) -> bool {
+fn is_tree_visible_in_allotment(tree_heigth: &u32, other_tree_heigths: &mut Vec<&u32>) -> bool {
     other_tree_heigths.sort();
 
     match other_tree_heigths.pop() {
